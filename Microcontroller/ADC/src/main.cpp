d52cfc12810ff07242b702c0a52f6669c2ed1435
mwlootens <43566398+mwlootens@users.noreply.github.com>
2018-11-20 10:50:06 +0100

    button interrupt

--- Microcontroller/ADC/src/main.cpp
+++ Microcontroller/ADC/src/main.cpp
@@ -1,15 +1,23 @@
 #include <Arduino.h>
 
 #define ANALOGPIN   A5
+#define BUTTONPIN 2
+
 
 void setup() {
     Serial.begin(9600);
     pinMode(ANALOGPIN, INPUT);
+    pinMode (BUTTONPIN,INPUT);
+    attachInterrupt(digitalPinToInterrupt(button), button, RISING);
 }
 
 
 void loop() {
+    // Empty loop
+}
+
+
+void button() {
     int value = analogRead(ANALOGPIN);
     Serial.println(value);
-    //  delay(2000);
 }
